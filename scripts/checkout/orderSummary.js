5ba793b9b951629fec19d201db1686c257854078
Mason Wang <wangyachenmason@163.com>
2024-06-15 17:16:14 -0700

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary(){ 
     let cartSummaryHTML = '';
@@ -118,6 +119,8 @@ export function renderOrderSummary(){
 
                 const container = document.querySelector(`.js-cart-item-container-${productId}`)
                 container.remove();
+
+                renderPaymentSummary();
             });
         });
 
@@ -126,7 +129,9 @@ export function renderOrderSummary(){
             element.addEventListener('click', ()=>{
                 const {productId, deliveryOptionId} = element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId);
+                
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
 }
